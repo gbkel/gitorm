a8a632b1bcd0dce7e3007d93c8c21d1fcdee034c
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-10-01 06:44:36 -0300

    setup CI test

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,8 +6,8 @@ describe('=> Repository connection', () => {
 	it('Should connect to github repository', async () => {
 		const Gitorm = new gitorm({
 			token: process.env.GIT_TOKEN,
-			repository: 'database',
-			owner: 'guilhermebkel'
+			repository: process.env.GIT_REPOSITORY,
+			owner: process.env.GIT_OWNER
 		})
 
 		await Gitorm.connect()
